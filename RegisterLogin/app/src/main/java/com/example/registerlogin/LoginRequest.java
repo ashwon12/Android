0e96ae86f02799813e4de5ad6875f516f5a5647c
김재원 <ahswon12@naver.com>
2020-04-23 01:07:06 +0900

    오류 수정

--- RegisterLogin/app/src/main/java/com/example/registerlogin/LoginRequest.java
+++ RegisterLogin/app/src/main/java/com/example/registerlogin/LoginRequest.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class LoginRequest extends StringRequest {
 
-    final static private String URL = "http://ashwon12.dothome.co.kr/Register.php";
+    final static private String URL = "http://ashwon12.dothome.co.kr/Login.php";
     private Map<String,String> map;
 
     public LoginRequest(String userID, String userPassword, Response .Listener<String>listener){
