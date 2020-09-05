20a15ef60487aada8a8f4c6a002e8251c47dd895
김재원 <ashwon12@naver.com>
2020-09-05 21:56:37 +0900

    카카오 로그인 구현
    카카오 디벨로퍼 사용

---
+++ quickstart-android/ImagePicker/app/src/main/java/com/example/imagepicker/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.imagepicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1888;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+
+}
