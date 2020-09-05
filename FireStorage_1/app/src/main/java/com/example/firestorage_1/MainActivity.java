03bac6db360e378dd0a7daaa4e27de22d7eee28a
김재원 <ashwon12@naver.com>
2020-09-06 02:39:49 +0900

    사진 가져와서 뿌리기

--- FireStorage_1/app/src/main/java/com/example/firestorage_1/MainActivity.java
+++ FireStorage_1/app/src/main/java/com/example/firestorage_1/MainActivity.java
@@ -1,14 +1,146 @@
 package com.example.firestorage_1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
+
+    private static final int PICK_IMAGE_REQUEST_CODE =1008;
+
+    private Button btn_getimage,btn_upload;
+    private EditText file_name;
+    private ProgressBar progressBar;
+    private TextView show_photo;
+    private ImageView get_image;
+
+    private Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_getimage=(Button)findViewById(R.id.btn_getphoto);
+        btn_upload=(Button)findViewById(R.id.btn_upload);
+        file_name=(EditText)findViewById(R.id.file_name);
+        get_image=(ImageView)findViewById(R.id.get_image);
+        progressBar=findViewById(R.id.proressbar);
+
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+
+        //사진 가져오기
+        btn_getimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChoose();
+            }
+        });
+
+        //사진 스토리지에 업로드
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadFile();
+            }
+        });
+
+
+
+    }
+
+    private String getFileExtensions(Uri uri){
+        ContentResolver contentResolver =getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadFile() {
+        if(image_uri != null){
+            StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                    +"."+getFileExtensions(image_uri));
+
+            fileReference.putFile(image_uri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setProgress(0);
+                                }
+                            },5000);
+
+                            Upload upload = new Upload(file_name.getText().toString().trim()
+                                    ,taskSnapshot.getUploadSessionUri().toString());
+                            String uploadId = databaseReference.push().getKey();
+                            databaseReference.child(uploadId).setValue(upload);
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this,"실패",Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0* taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                            progressBar.setProgress((int)progress);
+                        }
+                    });
+        }else{
+            Toast.makeText(this,"파일 선택 안됐음",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void openFileChoose() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,PICK_IMAGE_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST_CODE
+            ){
+            image_uri = data.getData();
+            Log.d("CommunityFragment", "img_uri = "+image_uri);
+
+            Picasso.get().load(image_uri).into(get_image);
+        }
     }
 }
