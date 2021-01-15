dc0e73e0f5dfc971b732172d4dc324ef1313bacf
김재원 <ashwon12@naver.com>
2021-01-16 01:36:43 +0900

    간단한 심리테스트앱

--- Kotlin/LoveTest/app/src/main/java/com/example/lovetest/MainActivity.kt
+++ Kotlin/LoveTest/app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    지금 정의하는 것이 아니라 나중에 정의하겠다는 의미 : lateinit var
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = nav_host_fragment.findNavController();
     }
 }
