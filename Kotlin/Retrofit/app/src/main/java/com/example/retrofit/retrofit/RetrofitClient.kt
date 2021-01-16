3822103aa4735058932448d6fe12456049124caf
김재원 <ashwon12@naver.com>
2021-01-17 02:50:23 +0900

    레트로핏 클라이언트

---
+++ Kotlin/Retrofit/app/src/main/java/com/example/retrofit/retrofit/RetrofitClient.kt
@@ -0,0 +1,26 @@
+package com.example.retrofit.retrofit
+
+import android.nfc.Tag
+import android.util.Log
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient{
+    //레트로핏 클라이언트 선언
+
+    private var retrofitClient : Retrofit?= null
+
+    //레트로핏 클라이언트 가져오기
+    fun getClient(baseUrl: String) : Retrofit? {
+        if(retrofitClient == null){
+
+            //레트로핏 빌더를 통해 인스턴스 생성
+            retrofitClient = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        return  retrofitClient
+    }
+}
