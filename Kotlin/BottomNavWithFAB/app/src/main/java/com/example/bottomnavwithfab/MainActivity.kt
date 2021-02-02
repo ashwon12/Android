29c98ddd710e0bd137c3b7d18bd134121d56c93b
김재원 <ashwon12@naver.com>
2021-02-02 22:21:06 +0900

    [Add] FAB 생성

--- Kotlin/BottomNavWithFAB/app/src/main/java/com/example/bottomnavwithfab/MainActivity.kt
+++ Kotlin/BottomNavWithFAB/app/src/main/java/com/example/bottomnavwithfab/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.bottomnavwithfab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.background=null
+        bottomNavigationView.menu.getItem(2).isEnabled=false
+
     }
 }
