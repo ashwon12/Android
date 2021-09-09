ecb6c0033eb79ddb52959cc13472d9342ead17ef
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2021-09-09 23:09:59 +0900

    [Add] MyViewModel Class
    - 뷰모델 생성
    - 뷰모델 내부에서 값이 변할때 호출되는 메서드(updateValue)

---
+++ Kotlin/LiveData/app/src/main/java/com/example/livedata/MyViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.livedata
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+enum class ActionType{
+    PLUS,MINUS
+}
+
+class MyViewModel : ViewModel() {
+
+    //mutableLiveData - 수정가능, 내부에서 사용
+    //LiveDate - 수정불가능, 읽기전용 외부에서 사용
+
+    private val _currentValue = MutableLiveData<Int>()
+    val currentValue : LiveData<Int>
+        get() = _currentValue
+
+    init {
+        Log.d("로그", "생성자 호출")
+        _currentValue.value = 0
+    }
+
+    fun updateValue(actionType: ActionType, input : Int){
+        //뷰모델 값을 변경시키는 메서드
+        when(actionType){
+            ActionType.PLUS ->
+                _currentValue.value = _currentValue.value?.plus(input)
+            ActionType.MINUS ->
+                _currentValue.value = _currentValue.value?.minus(input)
+        }
+    }
+
+}
+
